1acc1ede4a945ece4f6a54a7475659706bf3c6eb
Yaser <nightboy201@gmail.com>
2024-08-11 03:28:32 -0500

    fixed bootstrap import

--- BlazorMaui.UI/src/main.ts
+++ BlazorMaui.UI/src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import "bootstrap/dist/js/bootstrap";
+import "bootstrap";
 
 /*
     Wait for the webview to load Blazor Framework and start it if needed
